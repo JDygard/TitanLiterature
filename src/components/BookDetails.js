9ed478648e924e5187076db058b2d73cec4d811f
Joel Dygard <jdygard@gmail.com>
2024-09-02 21:51:18 +0200

    Test push

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -8,7 +8,8 @@ const BookDetails = () => (
     <h3>The debut novel of the Titan Chronology, “Children of Nemia” is available today</h3>
     <a href="#purchase" className="btn">Buy Now</a>
     </p>
-    <p class="body_text">
+    <p class="body_text"> 
+      ASPIOJDPOSDJPASODJASPODJASPDOJ
        Van is about to leave home on the journey of his lifetime. The pilgrimage marks a Nemian's coming of age
        and Van's been preparing for it since the day he was born, but he's about to learn the world is a much bigger 
        place than he was raised to believe-- and nothing could prepare him to face off against cosmic horrors from other worlds and the inconvenience of adolescent love, 
