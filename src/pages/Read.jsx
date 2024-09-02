bf85b376712df64d932d3442dc8406e30deda1ce
Joel Dygard <jdygard@gmail.com>
2024-09-02 22:35:45 +0200

    Add pdf viewer

---
+++ src/pages/Read.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import './Read.css';
+
+const Read = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const file = "/book.pdf"
+
+    return (
+        <div className="read-page">
+            <header className="read-header">
+                <h1>Titan - Chapter 1</h1>
+            </header>
+            <main className="pdf-container">
+                <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                    <Viewer
+                        fileUrl={file}
+                        plugins={[defaultLayoutPluginInstance]}
+                    />
+                </Worker>
+            </main>
+        </div>
+    );
+};
+
+export default Read;
